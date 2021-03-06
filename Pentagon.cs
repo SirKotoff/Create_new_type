24747d2cb1f9d6f1ac134853c5a9bc9748220170
SirKotoff <68810705+SirKotoff@users.noreply.github.com>
2021-03-06 23:35:07 +0300

    Добавьте файлы проекта.

---
+++ Pentagon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_create_new_type
+{
+    public class Pentagon
+    {
+
+        public double A { get; set; }
+        public double R { get; set; }
+        public double r { get; set; }
+        public double Ss { get; set; }
+        public double p { get; set; }
+
+
+
+        public void S()
+        {
+                //         S = a² / 4 * √ 25 + 10 * √5 
+                //        re = a / 10 * √ 50 + 10 * √5
+                //        ri = a / 10 * √ 25 + 10 * √5     
+              
+                Ss = Math.Pow(A, 2) / 4 * Math.Sqrt(25 + 10 * Math.Sqrt(5));
+                R = A / 10 * Math.Sqrt(50 + 10 * Math.Sqrt(5));
+                r = A / 10 * Math.Sqrt(25 + 10 * Math.Sqrt(5));
+           
+            
+            
+
+
+        }
+
+        public double P()
+        {
+            // формула рассчёта периметра пятиугольника равна a*5 где a-длина стороны, 5-количество углов 
+           p = A*5;
+            return p;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_create_new_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pentagon p = new Pentagon();
+
+
+ 
+                Console.WriteLine("Введите: Сторону A для рассчёта S,R,r");
+                p.A = Convert.ToDouble(Console.ReadLine());
+                if (p.A > 0)
+                {
+                    p.S();
+                    Console.WriteLine($"Радиус описанной окружности (R)= [{p.R}]");
+                    Console.WriteLine($"Радиус вписанной окружности (r)= [{p.r}]");
+                    Console.WriteLine($"Площадь пятиугольника = [{p.Ss}]");
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine($"Периметр пятиугольника (P)=[{p.P()}] ");
+                  
+                   
+                }
+                else
+                {
+                    Console.WriteLine("[Ошибка ввода: значение должно быть > 0]");
+                }
+              
+            
+            
+              
+
+
+
+            
+
+
+
+
+
+
+          
+        }
+    }
+}
